dadb2bba4971bda20a569a50ffea75845b2e3858
Nathan Kelly <nathanleokelly2008@gmail.com>
2023-11-07 19:14:21 +0100

    learing how to use arrays and got the fouratacwork

--- game.py
+++ game.py
@@ -6,18 +6,23 @@ healthPlayer = 100
 healthRobot = 200
 poisonDamage = 0
 biteDamage = 0
+hasBitten = False
+lastMessage = []
 
 def main(name): 
-    print("hellllo " + name)
+    global lastMessage
+    print("Hello " + name)
     while (healthPlayer > 0 and healthRobot > 0):
-#        os.system('clear')
+        os.system('clear')
         print("The robots health is " + str(healthRobot) )
         print("Watch out, your health is " + str(healthPlayer))
+        showAttack()
         print("It's your turn! Choose an option:")
         print("1. Attack                      (Deal 4 - 10 damage to robot)")
         print("2. Regenerate                  (Gain 8 healthpoints)")
         print("3. Poison                      (Add 4 to attack forever)")
         print("4. Serpents final strike       (Deal 2 times more damage than attack. One time use and loses you 2 hearts.)")
+        lastMessage = []
         move = input("> ")
         #bit weird anouncing what someone just inputted... keeping it for now.
         print("Your move is " + move)
@@ -37,29 +42,51 @@ def main(name):
     if (healthRobot < 0 and healthPlayer > 0):
         print("The robot has been slain!")
 
+def showAttack():
+    global lastMessage
+    result = "\n".join(lastMessage)
+    print("")
+    print(result)
+    print("")
+
+
 def attack():
+    global lastMessage
     global healthRobot
-    print("Uppercut!")
+    lastMessage.append("Your attack was an:")
     random_number = random.randint(4,10)
+    if random_number > 8:
+        lastMessage.append("Uppercut!")
+    else:
+        lastMessage.append("Light jab")
     damage = random_number + poisonDamage
     healthRobot = healthRobot - damage
 
 def regenerate():
+    global lastMessage
     global healthPlayer
     print("Im healing")
+    lastMessage.append("Uppercut!")
     healthPlayer = healthPlayer + 8
 
 def poison():
+    global lastMessage
     global poisonDamage
     poisonDamage += 4
     print(str(poisonDamage) + " damage points added to your attack from now on")
 
 def bite():
-    global biteDamage
-    random_number = random.randint(4,10)
-    biteDamage = (random_number + poisonDamage) *2
-    healthRobot = healthRobot - biteDamage
-    print("My last ditch effort, its now or never!")
+    global lastMessage
+    global biteDamage, healthRobot, hasBitten, healthPlayer
+    if not hasBitten:
+        random_number = random.randint(4,10)
+        biteDamage = (random_number + poisonDamage) *2
+        healthRobot = healthRobot - biteDamage
+        healthPlayer = healthPlayer - 7
+        print("My last ditch effort, its now or never!")
+        hasBitten = True
+    else:
+        print("You have already used Serpents final strike")
      
 
 main("nathan")
