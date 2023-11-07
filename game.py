0b19a58ab5a25ceb03d1d074c6a16cd776e1cbf2
Nathan Kelly <nathanleokelly2008@gmail.com>
2023-11-07 17:42:15 +0100

    bite function does not work

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ import random
 healthPlayer = 100
 healthRobot = 200
 poisonDamage = 0
-
+biteDamage = 0
 
 def main(name): 
     print("hellllo " + name)
@@ -14,10 +14,10 @@ def main(name):
         print("The robots health is " + str(healthRobot) )
         print("Watch out, your health is " + str(healthPlayer))
         print("It's your turn! Choose an option:")
-        print("1. Attack")
-        print("2. Regenerate")
-        print("3. Poison")
-        print("4. Bite")
+        print("1. Attack                      (Deal 4 - 10 damage to robot)")
+        print("2. Regenerate                  (Gain 8 healthpoints)")
+        print("3. Poison                      (Add 4 to attack forever)")
+        print("4. Serpents final strike       (Deal 2 times more damage than attack. One time use and loses you 2 hearts.)")
         move = input("> ")
         #bit weird anouncing what someone just inputted... keeping it for now.
         print("Your move is " + move)
@@ -27,6 +27,8 @@ def main(name):
             regenerate()
         elif move == "3":
             poison()
+        elif move == "4":
+            bite()
         else:
             print("I dont know that option")
     if (healthPlayer < 0 and healthRobot > 0):
@@ -50,16 +52,14 @@ def regenerate():
 def poison():
     global poisonDamage
     poisonDamage += 4
-    print(str(poisonDamage) + " damage points added to youer attack from now on")
+    print(str(poisonDamage) + " damage points added to your attack from now on")
 
- #   global healthPlayer
-  #  print("Im healing")
-
-
-#def bite():
- #   global healthPlayer
-  #  print("Im healing")
-   # healthPlayer = healthPlayer + 8
+def bite():
+    global biteDamage
+    random_number = random.randint(4,10)
+    biteDamage = (random_number + poisonDamage) *2
+    healthRobot = healthRobot - biteDamage
+    print("My last ditch effort, its now or never!")
      
 
 main("nathan")
