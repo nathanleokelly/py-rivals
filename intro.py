dc8a0f96188ad740060f018ba1a29fec7c2528f8
Nathan Kelly <nathanleokelly2008@gmail.com>
2023-11-10 18:03:00 +0100

    Created intro.py to contain all story

---
+++ intro.py
@@ -0,0 +1,14 @@
+import os
+
+def introduction():
+    print("I am an evil AI bot")
+    x = input("...")
+
+
+def robotWonOutro(gameInfo):
+    os.system('clear')
+    print("You have been defeated by the robot")
+
+def playerWonOutro(gameInfo):
+    os.system('clear')
+    print("The robot has been slain!") 
