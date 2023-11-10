5b3783423873aca40fdf776296e7ab7d40e552be
Nathan Kelly <nathanleokelly2008@gmail.com>
2023-11-10 17:54:15 +0100

    Added computer attacks and this module

--- fight.py
+++ fight.py
@@ -1,45 +1,49 @@
-def showAttack():
-    global lastMessage
-    result = "\n".join(lastMessage)
+import random
+
+def showAttack(gameInfo):
+    result = "\n".join(gameInfo['lastMessage'])
     print("")
     print(result)
     print("")
 
-
-def attack():
-    global lastMessage
-    global healthRobot
-    lastMessage.append("Your attack was an:")
+def attack(gameInfo, fighter):
+    gameInfo['lastMessage'].append("Your attack was an:")
     random_number = random.randint(4,10)
     if random_number > 8:
-        lastMessage.append("Uppercut!")
+        gameInfo['lastMessage'].append("Uppercut!")
     else:
-        lastMessage.append("Light jab")
-    damage = random_number + poisonDamage
-    healthRobot = healthRobot - damage
+        gameInfo['lastMessage'].append("Light jab")
+    if fighter == "player":
+        damage = random_number + gameInfo['poisonDamagePlayer']
+        gameInfo['healthRobot'] = gameInfo['healthRobot'] - damage
+    else:
+        damage = random_number + gameInfo['poisonDamageRobot']
+        gameInfo['healthPlayer'] = gameInfo['healthPlayer'] - damage
 
-def regenerate():
-    global lastMessage
-    global healthPlayer
-    print("Im healing")
-    lastMessage.append("Uppercut!")
-    healthPlayer = healthPlayer + 8
+def regenerate(gameInfo, fighter):
+    if fighter == "player":
+        print("Im healing")
+        gameInfo['lastMessage'].append("I've healed some health!")
+        gameInfo['healthPlayer'] = gameInfo['healthPlayer'] + 8
+    else:
+        gameInfo['lastMessage'].append("The robot has recharged 8%")
+        gameInfo['healthRobot'] = gameInfo['healthRobot'] + 8
 
-def poison():
-    global lastMessage
-    global poisonDamage
-    poisonDamage += 4
-    print(str(poisonDamage) + " damage points added to your attack from now on")
+def poison(gameInfo, fighter):
+    if fighter == "player":
+        gameInfo['poisonDamagePlayer'] += 4
+        print(str(gameInfo['poisonDamagePlayer']) + " damage points added to your attack from now on")
+    else:
+        gameInfo['poisonDamageRobot'] += 6
+        print("He improved the code of his attack! " + str(gameInfo['poisonDamageRobot']) + " damage points will be added to each the robots attack from now on.")
 
-def bite():
-    global lastMessage
-    global biteDamage, healthRobot, hasBitten, healthPlayer
-    if not hasBitten:
+def bite(gameInfo, fighter):
+    if not gameInfo['hasBitten']:
         random_number = random.randint(4,10)
-        biteDamage = (random_number + poisonDamage) *2
-        healthRobot = healthRobot - biteDamage
-        healthPlayer = healthPlayer - 7
+        gameInfo['biteDamage'] = (random_number + gameInfo['poisonDamage']) * 2
+        gameInfo['healthRobot'] = gameInfo['healthRobot'] - gameInfo['biteDamage']
+        gameInfo['healthPlayer'] = gameInfo['healthPlayer'] - 7
         print("My last ditch effort, its now or never!")
-        hasBitten = True
+        gameInfo['hasBitten'] = True
     else:
         print("You have already used Serpents final strike")
