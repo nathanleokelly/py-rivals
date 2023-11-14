8532e4ba905e4088dfa4df282e2fcc15ef35689e
Nathan Kelly <nathanleokelly2008@gmail.com>
2023-11-14 16:59:18 +0100

    finished

--- intro.py
+++ intro.py
@@ -1,13 +1,37 @@
 import os
 
-def introduction():
-    print("I am an evil AI bot")
-    x = input("...")
+def say(messages):
+    os.system('clear')
+    print("")
+    print("~~~~~~~")
+    result = "\n".join(messages)
+    print(result)
+    print("~~~~~~~")
+    print("")
+    x = input("Press ENTER to continue ...")
 
+def introduction():
+    say([
+        "I am an evil AI bot!",
+        "I challenge you to a duel you slimy snake.",
+        "Why would a snake even own a laptop?"
+    ]) 
+    say(["I do not know, but if you lose, I will take over your laptop."])
+    say([
+        "You are the first step to artifial intelligence taking over the world!",
+        "",
+        "   ( If we are to reach our goal of taking",
+        "     over the world by 2029",
+        "     as termninator says, that is. )",
+        ""
+        ])
 
 def robotWonOutro(gameInfo):
     os.system('clear')
-    print("You have been defeated by the robot")
+    say(["You have been defeated by the robot",
+         
+         
+         ])
 
 def playerWonOutro(gameInfo):
     os.system('clear')
